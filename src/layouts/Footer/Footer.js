8ae4180056f5db5b7503c05dce3a0eda1baa49ed
vanminhle <vanminh.le.754@proton.me>
2023-04-05 17:09:51 +0700

    T08: Done Footer and Fix Routes (Rebased)

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { FooterHome } from './Components/FooterHome/FooterHome';
+import { FooterDefault } from './Components/FooterDefault/FooterDefault';
 
 export default function Footer() {
-  return <FooterHome />;
+  const location = useLocation();
+  const isHome = location.pathname === '/';
+
+  return <>{isHome ? <FooterHome /> : <FooterDefault />}</>;
 }
