60a7f04840007c2a650b2545d1616f843f76a164
thanminhhy123 <thanminhhy@gmail.com>
2023-04-05 22:57:19 +0700

    Fix structure of components

--- src/pages/Departurement/pages/DepartmentDetail/DepartmentDetail.js
+++ src/pages/Departurement/pages/DepartmentDetail/DepartmentDetail.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import DepartmentIntroduction from '../../components/Department Introduction/departmentIntroduction';
 import DepartmentHealthCare from '../../components/DepartmentHealthCare/departmentHealthCare';
 export default function DepartmentDetail() {
   return (
     <>
-      <DepartmentIntroduction></DepartmentIntroduction>
-
       <DepartmentHealthCare></DepartmentHealthCare>
     </>
   );
