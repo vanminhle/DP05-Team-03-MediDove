86f74f8496a7fcc1d9afcec168e7421b8b72998a
dinhtrantienbao0409 <66706814+dinhtrantienbao0409@users.noreply.github.com>
2023-04-05 17:05:44 +0700

    feat: T12/setup API and create quote calculator (#3)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './index.css'
-import Header from './layouts/Header/Header'
-import Footer from './layouts/Footer/Footer'
-import { Outlet } from 'react-router-dom'
+import './index.css';
+import Header from './layouts/Header/Header';
+import Footer from './layouts/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Header />
-          <main>
-            <Outlet />
-          </main>
-      <Footer />
+      {/* <Header /> */}
+      <main>
+        <Outlet />
+      </main>
+      {/* <Footer /> */}
     </>
   );
 }
