e047ec234606c55265a69045bb08c5d3f9750436
scottnguyen0412 <scottnguyen0412@gmail.com>
2023-04-05 22:01:25 +0700

    T_04: fetch data team from api

--- src/pages/Home/components/sectionTeam/SectionTeam.js
+++ src/pages/Home/components/sectionTeam/SectionTeam.js
@@ -1,42 +1,63 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react';
 import './sectionTeam.css';
-import NurseIcon from '../../../../img/sectionTeam/section-back-icon.png'
-import LineTitleTeam from '../../../../img/sectionTeam/section-title-line.png'
+import NurseIcon from '../../../../img/sectionTeam/section-back-icon.png';
+import LineTitleTeam from '../../../../img/sectionTeam/section-title-line.png';
 import { Link } from 'react-router-dom';
+import { getTeams } from '../../../../apiServices/api';
 import CardTeam from './components/CardTeam';
 
 const SectionTeam = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await getTeams();
+    setData(response);
+    return response;
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <section className='team-area pt-115 pb-55'>
-      <div className='container'> 
+      <div className='container'>
         <div className='row'>
-            <div className='col-xl-6 col-lg-7 col-md-10'>
-                <div className='section-title-team pos-rel mb-75'>
-                    <div className='section-icon'>
-                        <img className='section-back-icon back-icon-left' src={NurseIcon}/>
-                    </div>
-                    <div className='section-text pos-rel'>
-                        <h5 className='heading5'>Our Team</h5>
-                        <h1 style={{paddingRight:'20px'}} className='heading1'>A Professional & Care Provider</h1>
-                    </div>
-                    <div className='section-line pos-rel'>
-                        <img className='' src={LineTitleTeam}/>
-                    </div>
-                </div>
+          <div className='col-xl-6 col-lg-7 col-md-10'>
+            <div className='section-title-team pos-rel mb-75'>
+              <div className='section-icon'>
+                <img className='section-back-icon back-icon-left' src={NurseIcon} />
+              </div>
+              <div className='section-text pos-rel'>
+                <h5 className='heading5'>Our Team</h5>
+                <h1 style={{ paddingRight: '20px' }} className='heading1'>
+                  A Professional & Care Provider
+                </h1>
+              </div>
+              <div className='section-line pos-rel'>
+                <img className='' src={LineTitleTeam} />
+              </div>
             </div>
-            <div className='col-xl-6 col-lg-5'>
-                <div className='section-button text-end d-none d-lg-block pt-80 team_btn'>
-                    <Link className='primary_btn btn-icon ml-0' to='/'>
-                        <span className='custom-span-plus'>+</span>
-                        Make Appointment
-                    </Link>
-                </div>
+          </div>
+          <div className='col-xl-6 col-lg-5'>
+            <div className='section-button text-end d-none d-lg-block pt-80 team_btn'>
+              <Link className='primary_btn btn-icon ml-0' to='/'>
+                <span className='custom-span-plus'>+</span>
+                Make Appointment
+              </Link>
             </div>
+          </div>
         </div>
-        <CardTeam/>
+        <div className='row'>
+            { 
+                data.map((item) => (
+                    <CardTeam key={item.id} name={item.name} role={item.role} 
+                        avatar={item.id}/>
+                ))
+            }
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SectionTeam
+export default SectionTeam;
--- src/pages/Home/components/sectionTeam/components/CardTeam.js
+++ src/pages/Home/components/sectionTeam/components/CardTeam.js
@@ -2,94 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Member1 from '../../../../../img/sectionTeam/avatar/member1.png'
 
-const CardTeam = () => {
+const CardTeam = ({name, role, avatar}) => {
   return (
-    <div className='row'>
+    <>
         <div className='col-xl-4 col-lg-4 col-md-6'>
             <div className='team-box text-center mb-60'>
                 <div className='team-thumb mb-45 pos-rel'>
-                    <img src={Member1}/>
+                    <img src={require(`../../../../../img/sectionTeam/avatar/member${avatar}.png`)}/>
                     <Link className='team-link'>
                         +
                     </Link>
                 </div>
                 <div className='team-content'>
-                    <h3 className='heading3'>Rosalina D. Williamson</h3>
-                    <h6 className='heading6'>Founder</h6>
+                    <h3 className='heading3'>{name}</h3>
+                    <h6 className='heading6'>{role}</h6>
                 </div>
             </div>
         </div>
-        <div className='col-xl-4 col-lg-4 col-md-6'>
-            <div className='team-box text-center mb-60'>
-                <div className='team-thumb mb-45 pos-rel'>
-                    <img src={Member1}/>
-                    <Link className='team-link'>
-                        +
-                    </Link>
-                </div>
-                <div className='team-content'>
-                    <h3 className='heading3'>Rosalina D. Williamson</h3>
-                    <h6 className='heading6'>Founder</h6>
-                </div>
-            </div>
-        </div>
-        <div className='col-xl-4 col-lg-4 col-md-6'>
-            <div className='team-box text-center mb-60'>
-                <div className='team-thumb mb-45 pos-rel'>
-                    <img src={Member1}/>
-                    <Link className='team-link'>
-                        +
-                    </Link>
-                </div>
-                <div className='team-content'>
-                    <h3 className='heading3'>Rosalina D. Williamson</h3>
-                    <h6 className='heading6'>Founder</h6>
-                </div>
-            </div>
-        </div>
-        <div className='col-xl-4 col-lg-4 col-md-6'>
-            <div className='team-box text-center mb-60'>
-                <div className='team-thumb mb-45 pos-rel'>
-                    <img src={Member1}/>
-                    <Link className='team-link'>
-                        +
-                    </Link>
-                </div>
-                <div className='team-content'>
-                    <h3 className='heading3'>Rosalina D. Williamson</h3>
-                    <h6 className='heading6'>Founder</h6>
-                </div>
-            </div>
-        </div>
-        <div className='col-xl-4 col-lg-4 col-md-6'>
-            <div className='team-box text-center mb-60'>
-                <div className='team-thumb mb-45 pos-rel'>
-                    <img src={Member1}/>
-                    <Link className='team-link'>
-                        +
-                    </Link>
-                </div>
-                <div className='team-content'>
-                    <h3 className='heading3'>Rosalina D. Williamson</h3>
-                    <h6 className='heading6'>Founder</h6>
-                </div>
-            </div>
-        </div>
-        <div className='col-xl-4 col-lg-4 col-md-6'>
-            <div className='team-box text-center mb-60'>
-                <div className='team-thumb mb-45 pos-rel'>
-                    <img src={Member1}/>
-                    <Link className='team-link'>
-                        +
-                    </Link>
-                </div>
-                <div className='team-content'>
-                    <h3 className='heading3'>Rosalina D. Williamson</h3>
-                    <h6 className='heading6'>Founder</h6>
-                </div>
-            </div>
-        </div>
-    </div>
+    </>
   )
 }
 
