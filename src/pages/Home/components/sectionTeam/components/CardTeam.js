bf2309b9980be1eb1fb70f80185d55b5c41168c2
Khoi <74314165+scottnguyen0412@users.noreply.github.com>
2023-04-05 16:56:18 +0700

    T_04: Create section team (#2)

---
+++ src/pages/Home/components/sectionTeam/components/CardTeam.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Member1 from '../../../../../img/sectionTeam/avatar/member1.png'
+
+const CardTeam = () => {
+  return (
+    <div className='row'>
+        <div className='col-xl-4 col-lg-4 col-md-6'>
+            <div className='team-box text-center mb-60'>
+                <div className='team-thumb mb-45 pos-rel'>
+                    <img src={Member1}/>
+                    <Link className='team-link'>
+                        +
+                    </Link>
+                </div>
+                <div className='team-content'>
+                    <h3 className='heading3'>Rosalina D. Williamson</h3>
+                    <h6 className='heading6'>Founder</h6>
+                </div>
+            </div>
+        </div>
+        <div className='col-xl-4 col-lg-4 col-md-6'>
+            <div className='team-box text-center mb-60'>
+                <div className='team-thumb mb-45 pos-rel'>
+                    <img src={Member1}/>
+                    <Link className='team-link'>
+                        +
+                    </Link>
+                </div>
+                <div className='team-content'>
+                    <h3 className='heading3'>Rosalina D. Williamson</h3>
+                    <h6 className='heading6'>Founder</h6>
+                </div>
+            </div>
+        </div>
+        <div className='col-xl-4 col-lg-4 col-md-6'>
+            <div className='team-box text-center mb-60'>
+                <div className='team-thumb mb-45 pos-rel'>
+                    <img src={Member1}/>
+                    <Link className='team-link'>
+                        +
+                    </Link>
+                </div>
+                <div className='team-content'>
+                    <h3 className='heading3'>Rosalina D. Williamson</h3>
+                    <h6 className='heading6'>Founder</h6>
+                </div>
+            </div>
+        </div>
+        <div className='col-xl-4 col-lg-4 col-md-6'>
+            <div className='team-box text-center mb-60'>
+                <div className='team-thumb mb-45 pos-rel'>
+                    <img src={Member1}/>
+                    <Link className='team-link'>
+                        +
+                    </Link>
+                </div>
+                <div className='team-content'>
+                    <h3 className='heading3'>Rosalina D. Williamson</h3>
+                    <h6 className='heading6'>Founder</h6>
+                </div>
+            </div>
+        </div>
+        <div className='col-xl-4 col-lg-4 col-md-6'>
+            <div className='team-box text-center mb-60'>
+                <div className='team-thumb mb-45 pos-rel'>
+                    <img src={Member1}/>
+                    <Link className='team-link'>
+                        +
+                    </Link>
+                </div>
+                <div className='team-content'>
+                    <h3 className='heading3'>Rosalina D. Williamson</h3>
+                    <h6 className='heading6'>Founder</h6>
+                </div>
+            </div>
+        </div>
+        <div className='col-xl-4 col-lg-4 col-md-6'>
+            <div className='team-box text-center mb-60'>
+                <div className='team-thumb mb-45 pos-rel'>
+                    <img src={Member1}/>
+                    <Link className='team-link'>
+                        +
+                    </Link>
+                </div>
+                <div className='team-content'>
+                    <h3 className='heading3'>Rosalina D. Williamson</h3>
+                    <h6 className='heading6'>Founder</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CardTeam
