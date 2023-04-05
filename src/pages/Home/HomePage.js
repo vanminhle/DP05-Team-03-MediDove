8f13daef4e298ad0e6091cbb6d60b320f346d2e4
scottnguyen0412 <scottnguyen0412@gmail.com>
2023-04-05 16:46:06 +0700

    T_04: Create section team

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Department from './components/sectionDepartsHome/SectionDepartment'
+import SectionTeam from './components/sectionTeam/SectionTeam'
 
 export default function HomePage() {
   return (
     <div>
       <Department/>
+      <SectionTeam/>
     </div>
   )
 }
--- src/pages/Home/components/index.js
+++ src/pages/Home/components/index.js
@@ -1,1 +1,2 @@
 export * from './sectionDepartsHome';
+export * from './sectionTeam';
