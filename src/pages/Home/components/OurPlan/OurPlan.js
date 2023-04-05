258b9aeddbd846f5c422d77c5412e43eff1ac83d
NguyenVanHuy1405 <vanhuy141520@gmail.com>
2023-04-05 22:52:01 +0700

    T06/SectionOurPlan

---
+++ src/pages/Home/components/OurPlan/OurPlan.js
@@ -0,0 +1,218 @@
+import React from 'react';
+import './ourPlan.css';
+const OurPlan = () => {
+  return (
+    <section className='pricing-area'>
+      <div className='container'>
+        <div className="row">
+
+          <div className='col-xl-5 col-lg-6 col-md-12'>
+
+            <div className='section-ourPlan-title'>
+              <div className='section-ourPlan-icons'>
+                <img
+                  className='section-ourPlan-back-icon back-icon-left'
+                  src='https://medi-dove.netlify.app/img/section/section-back-icon.png'
+                  alt='section-ourPlan-back-icon'
+                />
+              </div>
+
+              <div className="section-ourPlan-text">
+                <h5>Our Plan</h5>
+                <h1>Pricing & Plans</h1>
+              </div>
+
+              <div className="section-ourPlan-line">
+                <img src="https://medi-dove.netlify.app/img/shape/section-title-line.png"
+                     alt="section-line"/>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="col-xl-7 col-lg-6 col-md-12">
+
+            <div className="pricing-menu">
+                <ul className="nav nav-pills">
+                    <li className="nav-item">
+                        <button class="nav-link active"
+                         id="pills-home-tab"
+                         data-bs-toggle="tab"
+                         data-bs-target="#pills-home"
+                         data-toggle="pill"
+                         role="tab"
+                         aria-controls="pills-home" 
+                         aria-selected="true">monthly</button>
+                    </li>
+                    <li className="nav-item">
+                    <button class="nav-link"
+                     id="pills-profile-tab"
+                     data-bs-toggle="tab" 
+                     data-toggle="pill"
+                     data-bs-target="#pills-profile" 
+                     role="tab" aria-controls="pills-profile" 
+                     aria-selected="false">yearly</button>
+                    </li>
+                </ul>
+            </div>
+
+          </div>
+
+
+        </div>
+
+        <div className="row">
+            <div className="col-xl-12">
+                <div className="tab-content" id="pills-tabContent">
+
+                    <div className="tab-pane active"
+                     id="pills-home"
+                     role="tabpanel"
+                     aria-labelledby="pills-home-tab">
+                        <div className="row">
+                            <div className="col-xl-4 col-lg-4 col-md-6">
+                                <div className="pricing-box">
+                                    <div className="pricing-thumb">
+                                        <img src="https://medi-dove.netlify.app/img/pricing/pricing-thumb-1.png"/>
+                                    </div>
+                                    <div className="pricing-content">
+                                        <h1>Professional</h1>
+                                        <p>
+                                        Ut enim ad minim veniam,
+                                        quis istomw nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo.
+                                        </p>
+                                        <a className="primary_btn btn-icon ml-0">
+                                            <span>+</span>
+                                            Price: $
+                                            425.00
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-xl-4 col-lg-4 col-md-6">
+                                <div className="pricing-box">
+                                    <div className="pricing-thumb">
+                                        <img src="https://medi-dove.netlify.app/img/pricing/pricing-thumb-1.png"/>
+                                    </div>
+                                    <div className="pricing-content">
+                                        <h1>Professional</h1>
+                                        <p>
+                                        Ut enim ad minim veniam,
+                                        quis istomw nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo.
+                                        </p>
+                                        <a className="primary_btn btn-icon ml-0">
+                                            <span>+</span>
+                                            Price: $
+                                            425.00
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-xl-4 col-lg-4 col-md-6">
+                                <div className="pricing-box">
+                                    <div className="pricing-thumb">
+                                        <img src="https://medi-dove.netlify.app/img/pricing/pricing-thumb-1.png"/>
+                                    </div>
+                                    <div className="pricing-content">
+                                        <h1>Professional</h1>
+                                        <p>
+                                        Ut enim ad minim veniam,
+                                        quis istomw nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo.
+                                        </p>
+                                        <a className="primary_btn btn-icon ml-0">
+                                            <span>+</span>
+                                            Price: $
+                                            425.00
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+
+                    <div className="tab-pane"
+                     id="pills-profile"
+                     role="tabpanel"
+                     aria-labelledby="pills-home-tab">
+                        <div className="row">
+                            <div className="col-xl-4 col-lg-4 col-md-6">
+                                <div className="pricing-box">
+                                    <div className="pricing-thumb">
+                                        <img src="https://medi-dove.netlify.app/img/pricing/pricing-thumb-1.png"/>
+                                    </div>
+                                    <div className="pricing-content">
+                                        <h1>Professional1111</h1>
+                                        <p>
+                                        Ut enim ad minim veniam,
+                                        quis istomw nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo.
+                                        </p>
+                                        <a className="primary_btn btn-icon ml-0">
+                                            <span>+</span>
+                                            Price: $
+                                            425.00
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-xl-4 col-lg-4 col-md-6">
+                                <div className="pricing-box">
+                                    <div className="pricing-thumb">
+                                        <img src="https://medi-dove.netlify.app/img/pricing/pricing-thumb-1.png"/>
+                                    </div>
+                                    <div className="pricing-content">
+                                        <h1>Professional</h1>
+                                        <p>
+                                        Ut enim ad minim veniam,
+                                        quis istomw nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo.
+                                        </p>
+                                        <a className="primary_btn btn-icon ml-0">
+                                            <span>+</span>
+                                            Price: $
+                                            425.00
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-xl-4 col-lg-4 col-md-6">
+                                <div className="pricing-box">
+                                    <div className="pricing-thumb">
+                                        <img src="https://medi-dove.netlify.app/img/pricing/pricing-thumb-1.png"/>
+                                    </div>
+                                    <div className="pricing-content">
+                                        <h1>Professional</h1>
+                                        <p>
+                                        Ut enim ad minim veniam,
+                                        quis istomw nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo.
+                                        </p>
+                                        <a className="primary_btn btn-icon ml-0">
+                                            <span>+</span>
+                                            Price: $
+                                            425.00
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default OurPlan;
---
+++ src/pages/Home/components/TrustUs/TrustUs.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './TrustUs.css'
+const TrustUs = () => {
+  return (
+    <section className="cta-area-trust-us">
+        <div className="container">
+            <div className="row">
+                <div className="col-xl-10 offset-xl-1 col-md-12">
+                    <div className="cta-text-trust-us">
+                        <div className="section-title">
+                            <div className="section-text">
+                                <h5>Stay healthy & strong to enjoy life</h5>
+                                <h1>Trust Us To Be There To Help All & Make Things Well Again.</h1>
+                            </div>
+                        </div>
+
+                        <div className="section-button">
+                            <a className="btn-icon-green btn-icon" href="#">
+                                <span>+</span>
+                                get a consultant
+                            </a>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default TrustUs
