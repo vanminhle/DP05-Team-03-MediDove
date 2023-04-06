14c7e2e6cb38f8a8cebe5240ccf4d5f8d61189c1
vanminhle <vanminh.le.754@proton.me>
2023-04-06 20:02:12 +0700

    T20-22: Fix Conflict before Merge

--- src/pages/Doctor/pages/index.js
+++ src/pages/Doctor/pages/index.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export * from './DoctorDetail/DoctorDetail';
 export * from './DoctorOne/DoctorOne';
-=======
-export * from  './DoctorDetail';
->>>>>>> 5d959f9 (T20-22: Fix Styling and File Structure)
