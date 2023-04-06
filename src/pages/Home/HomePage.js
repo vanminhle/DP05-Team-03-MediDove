128837cc81ad308bc05e39b8fafb4d5a9a2a5e2e
Khoi <74314165+scottnguyen0412@users.noreply.github.com>
2023-04-06 15:28:15 +0700

    T 05/create section 24 7 (#14)
    * T_05: Create section available 247 in home
    
    * T_05: format code

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -4,11 +4,14 @@ import SectionTeam from './components/sectionTeam/SectionTeam';
 import OurPlan from './components/OurPlan/OurPlan';
 import TrustUs from './components/TrustUs/TrustUs';
 import New from './components/News/New';
+import SectionAvailable from './components/sectionAvailable247/SectionAvailable';
+
 export default function HomePage() {
   return (
     <div>
       <Department />
       <SectionTeam />
+      <SectionAvailable/>
       <OurPlan />
       <TrustUs />
       <New />
