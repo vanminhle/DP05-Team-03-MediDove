ce4f2e16c5f003258eb18bbecb52dedac90e13d4
dinhtrantienbao0409 <dinhtrantienbao0409@gmail.com>
2023-04-06 09:54:29 +0700

    fix: T18/download

--- src/pages/Departurement/Departurement.js
+++ src/pages/Departurement/Departurement.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { QuoteCalculator } from './components/Quote Calculator/quoteCalculator';
-import { FreeDownload } from './components/Free Download/download';
 
 export default function Departurement() {
   return (
     <div>
       <QuoteCalculator />
-      <FreeDownload />
     </div>
   );
 }
--- src/pages/Departurement/pages/DepartmentDetail/DepartmentDetail.js
+++ src/pages/Departurement/pages/DepartmentDetail/DepartmentDetail.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { FreeDownload } from '../../components/Free Download/download';
 
 export default function DepartmentDetail() {
-  return (
-    <div>DepartmentDetail</div>
-  )
+  return <FreeDownload />;
 }
