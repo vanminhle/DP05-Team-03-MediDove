85b76f1b2638b1ded28c9295f3c9feb748573c17
vanminhle <vanminh.le.754@proton.me>
2023-04-06 09:33:10 +0700

    T08: Footer Responsive Done

--- src/layouts/Footer/Components/FooterDefault/FooterDefault.js
+++ src/layouts/Footer/Components/FooterDefault/FooterDefault.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './style.css';
+import './../style.css';
 import HomeSingleFooterBlog from './HomeSingleFooterBlog';
 import FooterLogo from '../../../../assets/images/footer/footer-logo-3.png';
 
--- src/layouts/Footer/Components/FooterHome/FooterHome.js
+++ src/layouts/Footer/Components/FooterHome/FooterHome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './style.css';
+import './../style.css';
 import FooterLogo from '../../../../assets/images/footer/footer-logo.png';
 
 export const FooterHome = () => {
@@ -11,7 +11,7 @@ export const FooterHome = () => {
           <div className='container'>
             <div className='row'>
               <div className='col-xl-5 col-lg-6 col-md-8'>
-                <div className='footer-contact-info mb-3'>
+                <div className='footer-contact-info mb-4'>
                   <div className='emergency-call fix'>
                     <div className='emergency-call-icon float-start'>
                       <i className='fas fa-phone'></i>
