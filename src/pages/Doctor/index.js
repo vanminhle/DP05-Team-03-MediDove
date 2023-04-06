71c86995b1fd2009cc6bc8a8687e9638f9120390
Tshop <thienhngcd18649@fpt.edu.vn>
2023-04-06 13:46:15 +0700

    T20-22: Fix Styling and File Structure

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 export * from './DoctorOne';
 export * from './pages';
+=======
+export * from  './DoctorOne';
+export * from './pages'
+>>>>>>> 5d959f9 (T20-22: Fix Styling and File Structure)
