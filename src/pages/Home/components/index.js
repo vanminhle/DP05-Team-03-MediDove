b0f9896da81cddd09371250bbf583e41d160f8b8
NguyenVanHuy1405 <vanhuy141520@gmail.com>
2023-04-06 13:26:59 +0700

    fix format in T08/SectionNew

--- src/pages/Home/components/index.js
+++ src/pages/Home/components/index.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 export * from './sectionDepartsHome';
 export * from './sectionTeam';
 export * from './OurPlan';
 export * from './TrustUs';
-=======
-export * from './OurPlan';
-export * from './TrustUs';
 export * from './New';
->>>>>>> a291802 (feat:t06/sectionNew)
