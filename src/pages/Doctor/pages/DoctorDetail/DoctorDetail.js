4a491b6fd5f621a99e0e20d3b3c0fb6c974774d9
vanminh.le <66707859+vanminhle@users.noreply.github.com>
2023-04-07 10:48:21 +0700

    T26: Add map, fix doctor detail page (#21)

--- src/pages/Doctor/pages/DoctorDetail/DoctorDetail.js
+++ src/pages/Doctor/pages/DoctorDetail/DoctorDetail.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { DoctorLeft } from '../../components/doctorLeft';
 import { DoctorRight } from '../../components/doctorRight';
+import { ContactMap } from '../../components/ContactMap';
+import { HeaderDetail } from '../../components/HeaderDetail';
 import '../../components/doctorDetails.css';
 
 export default function DoctorDetail() {
   return (
-    <div className='container doctorDetailsArea'>
-      <div className='row'>
-        <div className='col-xl-7 col-lg-8'>
-          <DoctorLeft />
-        </div>
-        <div className='col-xl-5 col-lg-4'>
-          <DoctorRight />
+    <>
+      <HeaderDetail />
+      <div className='container doctorDetailsArea'>
+        <div className='row'>
+          <div className='col-xl-7 col-lg-7'>
+            <DoctorLeft />
+            <ContactMap />
+          </div>
+          <div className='col-xl-5 col-lg-5'>
+            <DoctorRight />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
