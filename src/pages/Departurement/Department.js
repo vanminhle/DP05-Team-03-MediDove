201cdb60e2337b7d650f40b1affbd893a9d772b9
thanminhhy <66708057+thanminhhy@users.noreply.github.com>
2023-04-07 11:05:42 +0700

    T13/section employer (#15)
    * Finish Feature
    
    * Fix the folder structure
    
    * Delete departmentDetail css file
    
    * Done T13/SectionEmployer

--- src/pages/Departurement/Department.js
+++ src/pages/Departurement/Department.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { QuoteCalculator } from './components/Quote Calculator/quoteCalculator';
 import DepartmentHealthCare from './components/DepartmentHealthCare/departmentHealthCare';
+import DepartmentSectionEmployer from './components/DepartmentSectionEmployer/departmentSectionEmployer';
 
 export default function Departurement() {
   return (
     <>
-      <QuoteCalculator />
       <DepartmentHealthCare></DepartmentHealthCare>
+      <QuoteCalculator />
+      <DepartmentSectionEmployer></DepartmentSectionEmployer>
     </>
   );
 }
