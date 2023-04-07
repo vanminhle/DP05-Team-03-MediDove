f86062f6a15a9b737dc7307b8cdbb80e696c40ae
Khoi <74314165+scottnguyen0412@users.noreply.github.com>
2023-04-07 10:39:37 +0700

    T_16: Create section dental care department (#20)
    * T_16: Create section dental care department
    
    * T_16: fetch data from api of our team in Doctor page

--- src/pages/Doctor/pages/DoctorOne/OurTeam/OurTeam.js
+++ src/pages/Doctor/pages/DoctorOne/OurTeam/OurTeam.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import image1 from '../../../../../img/doctor/section-title-line.png';
 import image2 from '../../../../../img/doctor/section-back-icon.png';
-import image3 from '../../../../../img/doctor/member1.png';
-import image4 from '../../../../../img/doctor/member2.png';
-import image5 from '../../../../../img/doctor/member3.png';
-import image6 from '../../../../../img/doctor/member4.png';
-import image7 from '../../../../../img/doctor/member5.png';
-import image8 from '../../../../../img/doctor/member6.png';
+import CardTeam from '../../../../Home/components/sectionTeam/components/CardTeam';
+import { getTeams } from '../../../../../apiServices/api';
 
 const OurTeam = () => {
+
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await getTeams();
+    setData(response);
+    return response;
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
       <section className='team-area pt-115 pb-55'>
@@ -38,7 +46,7 @@ const OurTeam = () => {
             </div>
           </div>
           <div className='row'>
-            <div className='col-xl-4 col-lg-4 col-md-6'>
+            {/* <div className='col-xl-4 col-lg-4 col-md-6'>
               <div className='team-box text-center mb-60'>
                 <div className='team-thumb mb-45 pos-rel'>
                   <img src={image3} alt='' />
@@ -51,77 +59,15 @@ const OurTeam = () => {
                   <h6>Founder</h6>
                 </div>
               </div>
-            </div>
-            <div className='col-xl-4 col-lg-4 col-md-6'>
-              <div className='team-box text-center mb-60'>
-                <div className='team-thumb mb-45 pos-rel'>
-                  <img src={image4} alt='' />
-                  <a className='team-link' href='/doctor/doctor_Detail'>
-                    +
-                  </a>
-                </div>
-                <div className='team-content'>
-                  <h3>Diconda PIran Will</h3>
-                  <h6>dentist</h6>
-                </div>
-              </div>
-            </div>
-            <div className='col-xl-4 col-lg-4 col-md-6'>
-              <div className='team-box text-center mb-60'>
-                <div className='team-thumb mb-45 pos-rel'>
-                  <img src={image5} alt='' />
-                  <a className='team-link' href='/doctor/doctorDetail'>
-                    +
-                  </a>
-                </div>
-                <div className='team-content'>
-                  <h3>Hulk M. Kenbon</h3>
-                  <h6>neurologist</h6>
-                </div>
-              </div>
-            </div>
-            <div className='col-xl-4 col-lg-4 col-md-6'>
-              <div className='team-box text-center mb-60'>
-                <div className='team-thumb mb-45 pos-rel'>
-                  <img src={image6} alt='' />
-                  <a className='team-link' href='/doctor/doctor_Detail'>
-                    +
-                  </a>
-                </div>
-                <div className='team-content'>
-                  <h3>Haliam Z. Dicolaz</h3>
-                  <h6>Consultant</h6>
-                </div>
-              </div>
-            </div>
-            <div className='col-xl-4 col-lg-4 col-md-6'>
-              <div className='team-box text-center mb-60'>
-                <div className='team-thumb mb-45 pos-rel'>
-                  <img src={image7} alt='' />
-                  <a className='team-link' href='/doctor/doctorDetail'>
-                    +
-                  </a>
-                </div>
-                <div className='team-content'>
-                  <h3>Nicolas D. Case</h3>
-                  <h6>dentist</h6>
-                </div>
-              </div>
-            </div>
-            <div className='col-xl-4 col-lg-4 col-md-6'>
-              <div className='team-box text-center mb-60'>
-                <div className='team-thumb mb-45 pos-rel'>
-                  <img src={image8} alt='' />
-                  <a className='team-link' href='/doctor/doctorDetail'>
-                    +
-                  </a>
-                </div>
-                <div className='team-content'>
-                  <h3>Phumdon H. Norman</h3>
-                  <h6>neurologist</h6>
-                </div>
-              </div>
-            </div>
+            </div> */}
+
+            {
+              data.map((item) => (
+
+                <CardTeam key={item.id} name={item.name} role={item.role} avatar={item.id} checkedPage="doctor"/>
+              ))
+            }
+            
           </div>
         </div>
       </section>
--- src/pages/Home/components/sectionTeam/components/CardTeam.js
+++ src/pages/Home/components/sectionTeam/components/CardTeam.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const CardTeam = ({ name, role, avatar }) => {
+const CardTeam = ({ name, role, avatar, checkedPage }) => {
   return (
     <>
       <div className='col-xl-4 col-lg-4 col-md-6'>
         <div className='team-box text-center mb-60'>
           <div className='team-thumb mb-45 pos-rel'>
-            <img src={require(`../../../../../img/sectionTeam/avatar/member${avatar}.png`)} />
+            <img src={checkedPage ? require(`../../../../../img/sectionTeam/avatar/member${avatar}.png`): 
+                      require(`../../../../../img/doctor/member${avatar}.png`)} />
             <Link className='team-link'>+</Link>
           </div>
           <div className='team-content'>
